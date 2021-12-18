6b13ec19ff858a2b7a1241c325e45c0c3a0207d8
Gabriel Lima <contact.gabss@gmail.com>
2021-12-17 23:57:47 -0300

    sera?

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // server.js
 let express = require('express')
 let serveStatic = require('serve-static')
-let history = require('connect-history-api-fallback')
+// let history = require('connect-history-api-fallback')
 let sslRedirect = require('heroku-ssl-redirect')
 
 let app = express()
 let port = process.env.PORT || 5000
 
 app.use(sslRedirect())
-app.use(history())
+// app.use(history())
 app.use(serveStatic(__dirname.concat('/dist')))
 
 app.listen(port)
